290d802134ec44566279436b118d212b31b6b1a7
Ummet Civi <ummetcivi@msn.com>
2018-05-27 11:11:15 +0300

    cross origin support added

--- src/main/java/com/ummetcivi/kalahgame/controller/GameController.java
+++ src/main/java/com/ummetcivi/kalahgame/controller/GameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @RestController
 @RequestMapping(path = "/api/game", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
 public class GameController {
 
     private static final String PLAYER_ID_HEADER = "Player-Id";
